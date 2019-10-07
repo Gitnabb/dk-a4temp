813c56935cbc5be2113bdfb63a4300b9fb95618f
Siggi Hallur <siggihallur2@gmail.com>
2019-10-07 10:38:44 +0200

    implementet server

--- Warmup/src/main/java/no/ntnu/datakomm/SimpleTcpServer.java
+++ Warmup/src/main/java/no/ntnu/datakomm/SimpleTcpServer.java
@@ -1,9 +1,19 @@
 package no.ntnu.datakomm;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
 /**
  * A Simple TCP client, used as a warm-up exercise for assignment A4.
  */
 public class SimpleTcpServer {
+    private static final int PORT = 1301;
+    
     public static void main(String[] args) {
         SimpleTcpServer server = new SimpleTcpServer();
         log("Simple TCP server starting");
@@ -12,9 +22,37 @@ public class SimpleTcpServer {
     }
 
     public void run() {
-        // TODO - implement the logic of the server, according to the protocol.
-        // Take a look at the tutorial to understand the basic blocks: creating a listening socket,
-        // accepting the next client connection, sending and receiving messages and closing the connection
+        try {
+            // TODO - implement the logic of the server, according to the protocol.
+            // Take a look at the tutorial to understand the basic blocks: creating a listening socket,
+            // accepting the next client connection, sending and receiving messages and closing the connection
+
+            ServerSocket welcomeSocket = new ServerSocket(PORT);
+            System.out.println("Connected to server on port " + PORT);
+            
+            Socket clientSocket = welcomeSocket.accept();
+            
+            InputStreamReader reader = new InputStreamReader(clientSocket.getInputStream());
+            BufferedReader bufReader = new BufferedReader(reader);
+            
+            String clientInput = bufReader.readLine();
+            System.out.println("Client: " + clientInput);
+            
+            String response = "Hei!";
+            
+            PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
+            writer.println(response);
+            
+            
+            // close client socket
+            clientSocket.close();
+
+            
+            // closing the welcome socket
+            welcomeSocket.close();
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
     }
 
     /**
